57d89ef20a18ce32ef17696215afbfbb9adfe8a7
muhohoweb <muhohokenya@users.noreply.github.com>
2024-03-06 22:49:58 +0300

    Add deploy confgi map

--- orderservice/obj/Debug/net8.0/orderservice.AssemblyInfo.cs
+++ orderservice/obj/Debug/net8.0/orderservice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("orderservice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6054180091d08fc30dbbdbbd560e6952a58db983")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04d3d1de5c8f5d61376623e8ce2907bf00a1af00")]
 [assembly: System.Reflection.AssemblyProductAttribute("orderservice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("orderservice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
